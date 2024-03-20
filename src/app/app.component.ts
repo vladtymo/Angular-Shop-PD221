461148beefde3a2afbe5051d3052a1b3f3be343e
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-03-20 15:49:56 +0200

    Create initial shop app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
   title = 'shop_pd221';
