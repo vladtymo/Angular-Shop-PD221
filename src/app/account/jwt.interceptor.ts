73ec32c438f8947d133ebe3480e0b86bfe0df869
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-04-03 17:46:25 +0300

    Show shackbar on server response error

--- src/app/account/jwt.interceptor.ts
+++ src/app/account/jwt.interceptor.ts
@@ -1,13 +1,18 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { TokenService } from '../services/token.service';
+import { catchError, throwError } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
 
   // ... additional logic ...
 
   const service = inject(TokenService);
+  const router = inject(Router);
   const token = service.getAccessToken();
+  const snackBar = inject(MatSnackBar);
 
   if (token) {
     req = req.clone({
@@ -15,5 +20,28 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error) => {
+      if (error && error instanceof HttpErrorResponse) {
+        switch (error.status) {
+          case 401:
+            // TODO: check RefreshToken
+            router.navigate(['/login']);
+            break;
+
+          default:
+            openSnackBar("Something went wrong!", 4);
+            break;
+        }
+
+      }
+      throw throwError(() => error);
+    }));
+
+  function openSnackBar(message: string, durationInM: number) {
+    snackBar.open(message, undefined, {
+      duration: durationInM * 1000,
+    });
+  }
 };
+
